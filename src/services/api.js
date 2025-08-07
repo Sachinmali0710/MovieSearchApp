10497cf59dcfec1ae242c23cdc44f2094617f405
Sachinmali0710 <sachinmali9331@gmail.com>
2025-08-07 17:12:15 +0530

    Initial commit for MovieSearchApp

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "770f5cc5ed0eaaa31785ecb656eba924";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
